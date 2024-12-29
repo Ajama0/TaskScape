f69ecccc0d49c0124ef90963d66eac478c055c6f
Ajama0 <abasjama04@hotmail.com>
2024-12-29 18:05:01 +0000

    define logic for binding data from client to request body parameter

--- client-side/src/app/Service/api/task.service.ts
+++ client-side/src/app/Service/api/task.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from './Models/Task';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(Httpclient: HttpClient) { }
+
+
+
+  createTasks(task:Task):Observable<Task>{
+  }
 }
