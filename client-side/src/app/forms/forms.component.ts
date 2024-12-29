a8a1c6f4210a50a8b43900bd9db5e43559b57602
Ajama0 <abasjama04@hotmail.com>
2024-12-29 17:10:17 +0000

    set up components and map entity return

---
+++ client-side/src/app/forms/forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.css'
+})
+export class FormsComponent {
+  
+
+}
--- server-side/src/main/java/com/TaskScape/TaskScape/TaskController.java
+++ server-side/src/main/java/com/TaskScape/TaskScape/TaskController.java
@@ -1,5 +1,10 @@
 package com.TaskScape.TaskScape;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/v1/Tasks")
 public class TaskController {
+
 }
