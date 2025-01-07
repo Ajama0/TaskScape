234fdedd81e2a34539fd18db96abb9d798f68803
Ajama0 <abasjama04@hotmail.com>
2025-01-07 20:01:47 +0000

    implement request of authenticating user

--- client-side/src/app/Service/api/TaskService/UserService/user.service.ts
+++ client-side/src/app/Service/api/TaskService/UserService/user.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Register } from '../../../../Models/Register';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  BASE_URL: string = "http://localhost:8097/api/v1/Auth";
+
+  constructor(private http:HttpClient) { 
+  }
+
+
+  public register(user:Register):Observable<string>{
+    /**
+     * this allows us to authenticate the user
+     * the user will recieve a jwt token in which will be used in the authorization header of subsequent requests
+     * 
+     */
+    const registerEndpoint:string = `${this.BASE_URL}/register`
+    return this.http.post<string>(registerEndpoint,user)
+
+
+
+  }
+
+
+
+
+
 }
